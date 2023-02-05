310f78e13262185b29a42dbb984248cf9967056d
SmartPolarBear <clevercoolbear@outlook.com>
2023-02-06 00:32:47 +0800

    add scanning logic

--- lexer/include/lexer/lexer.h
+++ lexer/include/lexer/lexer.h
@@ -7,6 +7,7 @@
 #include <lightlox/generator.h>
 
 #include <string>
+#include <utility>
 
 namespace lightlox
 {
@@ -14,37 +15,82 @@ namespace lightlox
 enum token_types
 {
 	// Single-character tokens.
-	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACKET, RIGHT_BRACKET, LEFT_BRACE, RIGHT_BRACE,
-	COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
-
+	TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+	TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,
+	TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,
+	TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR,
 	// One or two character tokens.
-	BANG, BANG_EQUAL,
-	EQUAL, EQUAL_EQUAL,
-	GREATER, GREATER_EQUAL,
-	LESS, LESS_EQUAL,
-
+	TOKEN_BANG, TOKEN_BANG_EQUAL,
+	TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,
+	TOKEN_GREATER, TOKEN_GREATER_EQUAL,
+	TOKEN_LESS, TOKEN_LESS_EQUAL,
 	// Literals.
-	IDENTIFIER, STRING, NUMBER,
-
+	TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
 	// Keywords.
-	AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-	PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+	TOKEN_AND, TOKEN_CLASS, TOKEN_ELSE, TOKEN_FALSE,
+	TOKEN_FOR, TOKEN_FUN, TOKEN_IF, TOKEN_NIL, TOKEN_OR,
+	TOKEN_PRINT, TOKEN_RETURN, TOKEN_SUPER, TOKEN_THIS,
+	TOKEN_TRUE, TOKEN_VAR, TOKEN_WHILE,
 
-	END
+	TOKEN_ERROR, // TOKEN_EOF
 };
 
 struct source_information
 {
 	int line, column;
+
+	source_information(int _line, int _column)
+		: line(_line), column(_column)
+	{
+	}
 };
 
 struct token
 {
 	source_information src;
-	token_types token;
+	token_types type;
 	std::string lexeme;
+
+	token(source_information _src, token_types _type, std::string _lexeme)
+		: src(_src), type(_type), lexeme(std::move(_lexeme))
+	{
+	}
+
+	static token error(source_information src, std::string msg);
 };
 
-generator<token> scan_all_tokens(const std::string &src);
+class scanner
+{
+public:
+	explicit scanner(std::string code);
 
+	generator<token> scan_all_tokens();
+private:
+	token make_token(token_types type);
+
+	token scan_next_token();
+
+	char advance();
+
+	char peek(int len=0);
+
+	void skip_whitespaces();
+
+	void skip_comments();
+
+	void new_line();
+
+	bool match(char expected);
+
+	bool at_end();
+
+
+	std::string code_{};
+
+	std::string::const_iterator start_;
+	std::string::const_iterator current_;
+
+	int col_{};
+	int line_{};
+};
 }
--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -4,37 +4,162 @@
 
 #include <lexer/lexer.h>
 
+#include <utility>
+
 using namespace std;
 using namespace lightlox;
 
-#define SINGLE_CHAR_TOKEN(type, ch) \
-case ch: \
-    col++;                              \
-    lexeme.clear();                      \
-    lexeme+=ch;\
-    co_yield token{source_information{col, line},\
-        type, \
-        lexeme};                        \
-break                               \
-
-lightlox::generator<lightlox::token> lightlox::scan_all_tokens(const std::string &src)
+token token::error(source_information src, std::string msg)
 {
-	string lexeme;
-	int col = 0, line = 0;
-	for (auto it = src.begin(); it != src.end();)
+	return token{src, TOKEN_ERROR, std::move(msg)};
+}
+
+scanner::scanner(std::string code)
+	: code_(std::move(code)),
+	  start_(code_.begin()),
+	  current_(code_.begin())
+{
+}
+
+generator<token> scanner::scan_all_tokens()
+{
+	while (start_ <= current_ && !at_end())
 	{
-		char c = *it++;
-		switch (c)
-		{
-		SINGLE_CHAR_TOKEN(token_types::LEFT_PAREN, '(');
-		SINGLE_CHAR_TOKEN(token_types::RIGHT_PAREN, ')');
-		SINGLE_CHAR_TOKEN(token_types::LEFT_BRACKET, '[');
-		SINGLE_CHAR_TOKEN(token_types::RIGHT_BRACKET, ']');
-		SINGLE_CHAR_TOKEN(token_types::LEFT_BRACE, '{');
-		SINGLE_CHAR_TOKEN(token_types::RIGHT_BRACE, '}');
-		default:
-			break;
-		}
+		auto token = scan_next_token();
+		co_yield token;
 	}
 	co_return;
 }
+
+token scanner::scan_next_token()
+{
+	skip_whitespaces();
+	start_ = current_;
+
+	char c = advance();
+
+	switch (c)
+	{
+	case '(':
+		return make_token(TOKEN_LEFT_PAREN);
+	case ')':
+		return make_token(TOKEN_RIGHT_PAREN);
+	case '{':
+		return make_token(TOKEN_LEFT_BRACE);
+	case '}':
+		return make_token(TOKEN_RIGHT_BRACE);
+	case ';':
+		return make_token(TOKEN_SEMICOLON);
+	case ',':
+		return make_token(TOKEN_COMMA);
+	case '.':
+		return make_token(TOKEN_DOT);
+	case '-':
+		return make_token(TOKEN_MINUS);
+	case '+':
+		return make_token(TOKEN_PLUS);
+	case '/':
+		return make_token(TOKEN_SLASH);
+	case '*':
+		return make_token(TOKEN_STAR);
+
+	case '!':
+		return make_token(
+			match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+	case '=':
+		return make_token(
+			match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+	case '<':
+		return make_token(
+			match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+	case '>':
+		return make_token(
+			match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+	}
+
+	return token::error(source_information(line_, col_), "Unexpected character.");
+}
+
+token scanner::make_token(token_types type)
+{
+	string lexeme{start_, current_ + 1};
+
+	return {source_information(line_, col_),
+			type,
+			lexeme};
+}
+char scanner::advance()
+{
+	col_++;
+	return *current_++;
+}
+
+void scanner::new_line()
+{
+	line_++;
+	col_ = 0;
+}
+
+bool scanner::match(char expected)
+{
+	if (at_end())
+	{
+		return false;
+	}
+	if (*current_ != expected)
+	{
+		return false;
+	}
+	current_++;
+	return true;
+}
+void scanner::skip_whitespaces()
+{
+	while (!at_end())
+	{
+		auto c = peek();
+		switch (c)
+		{
+		case ' ':
+		case '\r':
+		case '\t':
+			advance();
+			break;
+		case '\n':
+			new_line();
+			advance();
+			break;
+		case '/':
+			skip_comments();
+			break;
+		default:
+			return;
+		}
+	}
+}
+
+void scanner::skip_comments()
+{
+	if (peek(1))
+	{
+		while (peek() != '\n' && !at_end())
+		{
+			advance();
+		}
+	}
+}
+
+char scanner::peek(int len)
+{
+	if (len > code_.end() - current_)
+	{
+		return 0;
+	}
+	return *(current_ + len);
+}
+
+bool scanner::at_end()
+{
+	return current_ == code_.end();
+}
+
